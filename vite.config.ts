15c4f3b7fde67a14d6f39e6c7d8ded321799faf2
Itarim <itarim_system@MacBook-Air-Itarim.local>
2026-06-03 13:12:17 +0300

    1_0_7_version

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') }
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) }
+  },
+  build: {
+    target: 'es2015',
+    cssTarget: 'chrome61',
   },
   server: {
     proxy: {
